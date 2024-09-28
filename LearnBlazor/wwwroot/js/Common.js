eb84b59276d1062f85dd48ed0e557d0d755a8edd
Brandon Davis <wowzerss90@yahoo.com>
2024-09-28 18:14:20 -0400

    Installed js packages for messages and used the JSRuntime to call js functions. Referenced components so i can use their public functions.

---
+++ LearnBlazor/wwwroot/js/Common.js
@@ -0,0 +1,25 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message)
+    }
+    if (type == "error") {
+        toastr.error(message)
+    }
+}
+
+window.showSweetalert = function (type, message) {
+    if (type === "success") {
+        Swal.fire({
+            title: "Good Job!",
+            text: message,
+            icon: "success"
+        });
+    }
+    if (type === "error") {
+        Swal.fire({
+            title: "Something went wronge!",
+            text: message,
+            icon: "error"
+        });
+    }
+}
