4c302f8dae28c84132081836d2a8a8182f29c667
Brandon Davis <wowzerss90@yahoo.com>
2026-02-16 00:34:19 -0500

    Blazor Lifecycle Methods

--- LearnBlazor/obj/Debug/net10.0/LearnBlazor.AssemblyInfo.cs
+++ LearnBlazor/obj/Debug/net10.0/LearnBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50aa8ee2a19b47806f83565d58f030c19ace7c8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3581012326f7a7e771b63556d2806653398358")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
